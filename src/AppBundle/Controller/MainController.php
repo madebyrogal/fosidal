41fe5043334ebe5fe47d1d4e61012931c968bf2f
madebyrogal <tomasz@madebyrogal.com>
2016-01-17 14:07:26 +0100

    #ADD Structure of main controler

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class MainController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @Method({"GET","HEAD"})
+     */
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Main:index.html.twig', array(
+            // ...
+        ));
+    }
+
+}
