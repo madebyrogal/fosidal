63286f5b803a00009e57b860ec387c298cfae25a
madebyrogal <tomasz@madebyrogal.com>
2016-02-28 12:28:34 +0100

    #ADD Filters to Result

--- src/AdminBundle/Controller/ResultController.php
+++ src/AdminBundle/Controller/ResultController.php
@@ -18,17 +18,29 @@ class ResultController extends Controller
     /**
      * Lists all Result entities.
      *
-     * @Route("/", name="admin_result_index")
+     * @Route("/{quize}", name="admin_result_index")
      * @Method("GET")
      */
-    public function indexAction()
+    public function indexAction($quize = 0)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $results = $em->getRepository('AdminBundle:Result')->findBy(array(), array('survey'=>'ASC', 'points' => 'DESC', 'created' => 'ASC'));
-
+        if($quize) {
+            $survey = $em->getRepository('AdminBundle:Survey')->find($quize);
+        } else {
+            $survey = $em->getRepository('AdminBundle:Survey')->findActive();
+        }
+        $surveys = $em->getRepository('AdminBundle:Survey')->findBy(array(), array('start'=>'ASC'));
+        $results = $em->getRepository('AdminBundle:Result')->findBy(array('survey'=>$survey), array('survey'=>'ASC', 'points' => 'DESC', 'created' => 'ASC'));
+        $awards1 = $em->getRepository('AdminBundle:Result')->findBy(array('award1'=> true, 'survey'=>$survey));
+        $awards2 = $em->getRepository('AdminBundle:Result')->findBy(array('award2'=> true, 'survey'=>$survey));
+        //dump($survey);die;
         return $this->render('result/index.html.twig', array(
+                    'quize' => $survey,
+                    'surveys' => $surveys,
                     'results' => $results,
+                    'award1Count'=>count($awards1),
+                    'award2Count'=>count($awards2)
         ));
     }
 
@@ -49,32 +61,32 @@ class ResultController extends Controller
     /**
      * Add award 1
      *
-     * @Route("/award1/{id}", name="admin_result_award1_add")
+     * @Route("/{quize}/award1/{id}", name="admin_result_award1_add")
      * @Method("GET")
      */
-    public function award1Action(Result $result)
+    public function award1Action(Result $result, $quize)
     {
         $result->setAward1(true);
         $em = $this->getDoctrine()->getManager();
         $em->persist($result);
         $em->flush();
         
-        return $this->forward('AdminBundle:Result:index');
+        return $this->redirectToRoute('admin_result_index', array('quize'=>$quize));
     }
     
     /**
      * Remove award 1
      *
-     * @Route("/award1_remove/{id}", name="admin_result_award1_remove")
+     * @Route("/{quize}/award1_remove/{id}", name="admin_result_award1_remove")
      * @Method("GET")
      */
-    public function award1RemoveAction(Result $result){
+    public function award1RemoveAction(Result $result, $quize){
         $result->setAward1(false);
         $em = $this->getDoctrine()->getManager();
         $em->persist($result);
         $em->flush();
         
-        return $this->forward('AdminBundle:Result:index');
+        return $this->redirectToRoute('admin_result_index', array('quize'=>$quize));
     }
 
     /**
--- web/assets/js/panel.js
+++ web/assets/js/panel.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
     initTinyMCE();
+    changeEditionInResult();
 });
 
+function changeEditionInResult(){
+    $('#quizeEdition').change(function(){
+       var uri = $('#adminResultIndexURL').val();
+       window.location.href = uri + '/'+ $(this).val();
+    });
+}
 
 function initTinyMCE() {
     tinymce.init({
